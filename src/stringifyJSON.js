9aa28a9171f9ec5a7baf5221b273d04430fa5e96
Bradford Melluish <bradmelluish@gmail.com>
2016-07-04 13:04:56 -0700

    Created switch statement for each stringify case, with default returning undefined and array and object cases recursively handling nested objects and arrays.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log(typeof obj)
+  switch(typeof obj) {
+  	case 'boolean':
+  	case 'number':
+  		return ''+obj;
+	case 'string':
+		return '\"' + obj + '\"';
+	case 'object':
+		var str = '';
+		if(Array.isArray(obj)) {
+			if(obj.length === 0) {
+				return '[]'
+			} else {
+				obj.forEach(x => { 
+					var check;
+					if(check = stringifyJSON(x))
+						str += check + ','; 
+				})
+				str = str.slice(0, str.length - 1); 
+				str ='[' + str + ']';
+			}
+		} else if(obj === null) {
+			return ''+null;
+		} else {
+			for(var each in obj) {
+				var check;
+				if(check = stringifyJSON(obj[each]))
+					str += '\"' + each + '\":' + check + ',';
+			}
+			if(str.length > 2)
+				str = str.slice(0, str.length - 1);
+			str = '{' + str + '}';
+		}
+		return str;
+	default: 
+		return;
+  }
 };
