fdaf1f3f6319edfda1cda77532d742e881e321e8
Bradford Melluish <bradmelluish@gmail.com>
2016-07-04 15:52:43 -0700

    Implemented getElementsByClassName by recursively checking each child and pushing nodes containing the target class onto the results array. Had to filter out #text nodes as they contain no classes and were causing errors by returning undefined with element.classList.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,14 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  var results = [];
+  function check(node){
+	if(node.classList.value.split(' ').indexOf(className) !== -1)
+		results.push(node);
+	node.childNodes.forEach(child => {if(child.nodeName !== '#text') check(child)});
+  }
+  check(document.body);
+  return results;
 };
